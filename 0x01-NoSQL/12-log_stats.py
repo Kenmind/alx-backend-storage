e85ffa706c32d141b6d26d1fc1ff50ed70a4a956
Kenmind <kennedykalaluka04@gmail.com>
2022-07-05 18:56:57 +0200

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Provides stats about Nginx logs restored in mongoDB"""
 import pymongo as pm
+import pprint
 
 db = pm.MongoClient()
 mydb = db["logs"]
@@ -14,6 +15,7 @@ get_patch = mycol.count_documents({"method": "PATCH"})
 get_delete = mycol.count_documents({"method": "DELETE"})
 get_total = mycol.count_documents({})
 get_status = mycol.count_documents({"method": "GET", "path": "/status"})
+get_all = mycol.find({})
 
 
 print("{} logs".format(get_total))
@@ -21,3 +23,5 @@ print("Methods:\n\tmethod GET: {}\n\tmethod POST: {}\n\tmethod PUT\
 : {}\n\tmethod PATCH: {}\n\tmethod DELETE: {}\n{} status \
 check".format(get_get, get_post, get_put,
               get_patch, get_delete, get_status))
+print("IPs:\n\t{}".format(get_all))
+pprint(get_all)
